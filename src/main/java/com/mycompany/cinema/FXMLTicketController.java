3951b1bb3e941798092da53a232229ebd03afee1
knocheg <43085046+knocheg@users.noreply.github.com>
2018-10-14 17:48:34 +0200

    Added DB connection to the Ticket Chooser so the base (adult) price would be read from the database, fixed problem with the getter for the Showid in the time Scene

--- src/main/java/com/mycompany/cinema/FXMLTicketController.java
+++ src/main/java/com/mycompany/cinema/FXMLTicketController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import java.sql.*;
 
 /**
  *
@@ -146,6 +147,7 @@ public class FXMLTicketController implements Initializable {
     //INITIALIZE------------------------------------------>
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        databaseSourceGarrettShowPrice();
         ticketMovie.setText(getMovie());//set movie title
         ticketDate.setText(date.getTitleDate());//set date title
         ticketTime.setText(getTime());//set time title
@@ -159,7 +161,6 @@ public class FXMLTicketController implements Initializable {
 
     //Set Prices--------------------------------------------------->
     public void setDefaulPrices() {
-        adultPrice = 10.00;
         priceOne.setText("$" + Double.toString(adultPrice));
         seniorPrice = adultPrice - seniorDiscount*adultPrice;
         priceTwo.setText("$" + Double.toString(seniorPrice));
@@ -199,6 +200,51 @@ public class FXMLTicketController implements Initializable {
     public String getPriceString() {
         String price = String.format("$%.2f", total);
         return price;
-    }
 
-}
+    }
+    
+ public void databaseSourceGarrettShowPrice() {
+        int count = 0;
+        String showID;
+                //Registering the Driver
+        try {
+            Driver myDriver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(myDriver);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            System.out.println("Driver Not Registered - Check you mysql-connector file is located under the CLASSPATH OF JDK");
+        }
+        ///Connecting to the Garrett server -database-
+        try {
+            //database credentials
+            String URL = "jdbc:mysql://group-g.g-knoche.com/CINEMA";
+            String USER = "rafael";
+            String PASS = "rafaelcmsc495";
+            String SQLTORUN = ""; //this is the string we will build the query in            
+            //stablish connection
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            Statement stmt = conn.createStatement(); //set statements
+            SQLTORUN = "SELECT SHOW_PRICE FROM SHOWS WHERE SHOW_ID = ";//sql statement with all but the 
+            SQLTORUN = SQLTORUN + "'" + time.getshowIDString() + "'" + ";"; // need to get the SHOW_ID for the showing selected by the customer
+            System.out.println(SQLTORUN);
+            ResultSet rs = stmt.executeQuery(SQLTORUN);//compile and execute statements
+            //setting the button texts with movies as it scans the database;
+            while (rs.next()) {
+                count += 1;
+                adultPrice = rs.getDouble(1);  // only one column and that is show_priced
+                //setting text to buttons and movie texts
+                if (count == 1) {
+                    //set Varialble
+                adultPrice = rs.getDouble(1);
+                } else if (count == 2) {  //Should never get here as there should only be one entry per show_id.  more for diagnostics than anything
+                    //set Varialble
+                    System.out.println("Something went very wrong SHOW_ID should be unique");
+                
+                }
+            }
+        }
+        catch (SQLException ex) {
+        System.out.println(ex);		
+                             }
+			}
+		}
